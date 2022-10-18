18237c524be027aa3216c691d3e23dd605bf38ec
Alexandre Bourget <alex@bourget.cc>
2022-10-18 03:16:57 +0100

    Added UnsubscribeURL only

--- hermes.go
+++ hermes.go
@@ -49,6 +49,7 @@ type Product struct {
 	Logo        string // e.g. https://matcornic.github.io/img/logo.png
 	Copyright   string // Copyright © 2019 Hermes. All rights reserved.
 	TroubleText string // TroubleText is the sentence at the end of the email for users having trouble with the button (default to `If you’re having trouble with the button '{ACTION}', copy and paste the URL below into your web browser.`)
+	UnsubscribeURL string
 }
 
 // Email is the email containing a body
